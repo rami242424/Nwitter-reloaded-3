e8c0a0eaac3210b5989a6d120f0ad0022a58432c
Rami <138556019+rami242424@users.noreply.github.com>
2025-12-10 10:00:00 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Layout from "./components/layout";
 import Home from "./routes/home";
 import Profile from "./routes/profile";
+import Login from "./routes/login";
 
 
 const router = createBrowserRouter([
@@ -18,6 +19,10 @@ const router = createBrowserRouter([
         element: <Profile />
       }
     ]
+  },
+  {
+    path: "/login",
+    element: <Login />
   }
 ])
 
