743a0a5cf2328aca639eba0208811800fe4a9dd8
Rami <138556019+rami242424@users.noreply.github.com>
2025-12-11 10:00:00 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Layout from "./components/layout";
 import Home from "./routes/home";
 import Profile from "./routes/profile";
 import Login from "./routes/login";
+import CreateAccount from "./routes/create-account";
 
 
 const router = createBrowserRouter([
@@ -23,6 +24,10 @@ const router = createBrowserRouter([
   {
     path: "/login",
     element: <Login />
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount />
   }
 ])
 
