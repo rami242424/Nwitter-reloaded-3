8dec618ff787507d7e862e9f021ad3f97271e511
Rami <138556019+rami242424@users.noreply.github.com>
2025-12-13 10:00:00 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,27 @@ import Home from "./routes/home";
 import Profile from "./routes/profile";
 import Login from "./routes/login";
 import CreateAccount from "./routes/create-account";
-import { createGlobalStyle } from "styled-components";
-import reset from "styled-reset";
+import { createGlobalStyle } from 'styled-components';
+import { reset } from 'styled-reset';
 
+const GlobalStyle = createGlobalStyle`
+${reset};
+*{
+  box-sizing: border-box;
+}
+body {
+  background-color: black;
+  color: white;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+`;
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    children: [
+    children : [
       {
         path: "",
         element: <Home />
@@ -33,22 +45,10 @@ const router = createBrowserRouter([
   }
 ]);
 
-const GlobalStyles = createGlobalStyle`
-  ${reset};
-  * {
-    box-sizing: border-box;
-  }
-  body {
-    background-color: black;
-    color: white;
-    font-family : system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-`;
-
 function App() {
   return <>
-    <GlobalStyles />
-    <RouterProvider router={router}/>
+    <GlobalStyle />
+    <RouterProvider router={router} />
   </>
 }
 
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,8 @@
 import { Outlet } from "react-router-dom";
 
 export default function Layout(){
-    return (
-        <>
-            <h1>저는 레이아웃입니다.</h1>
-            <Outlet />
-        </>
-    );
+    return <>
+        <h1>레이아웃</h1>
+        <Outlet />
+    </>
 }
