7850bd3251eac9175c495a07afd96c2a58d39724
Rami <138556019+rami242424@users.noreply.github.com>
2025-12-19 10:00:00 +0900

    commit

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,3 +1,18 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div``;
+const Form = styled.form``;
+const Input = styled.input``;
+
 export default function CreateAccount(){
-    return <h1>Create-Account</h1>
+    return (
+        <Wrapper>
+            <Form>
+                <Input name="name" placeholder="Name" type="text" required/>
+                <Input name="email" placeholder="Email" type="email" required/>
+                <Input name="password" placeholder="Password" type="password" required/>
+                <Input type="submit" value="Create Account"/>
+            </Form>
+        </Wrapper>
+    );
 }
