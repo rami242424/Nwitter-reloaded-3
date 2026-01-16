6fadca53e8149f980f475ae1b1d64299e846b144
Rami <138556019+rami242424@users.noreply.github.com>
2026-01-16 10:00:00 +0900

    commit

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,14 +5,31 @@ const Wrapper = styled.div`
     display: grid;
     gap: 50px;
     grid-template-columns: 1fr 4fr;
+    height: 100%;
     padding: 50px 0px;
     width: 100%;
     max-width: 860px;
 `;
 
-const Menu = styled.div``;
+const Menu = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+`;
 
-const MenuItem = styled.div``;
+const MenuItem = styled.div`
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 2px solid white;
+    height: 50px;
+    width: 50px;
+    border-radius: 50%;
+
+    
+`;
 
 export default function Layout(){
     return <>
