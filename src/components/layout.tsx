0c3418249c6541f7f6ac1a3a6b7c2ecf89bfe5f7
Rami <138556019+rami242424@users.noreply.github.com>
2026-01-17 10:00:00 +0900

    commit

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -27,8 +27,16 @@ const MenuItem = styled.div`
     height: 50px;
     width: 50px;
     border-radius: 50%;
-
-    
+    svg {
+        width: 30px;
+        fill: white;
+    }
+    &.log-out {
+        border-color: tomato;
+        svg {
+            fill: tomato;
+        }
+    }
 `;
 
 export default function Layout(){
@@ -49,7 +57,7 @@ export default function Layout(){
                         </svg>
                     </MenuItem>
                 </Link>    
-                <MenuItem>
+                <MenuItem className="log-out">
                     <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                         <path clipRule="evenodd" fillRule="evenodd" d="M3 4.25A2.25 2.25 0 0 1 5.25 2h5.5A2.25 2.25 0 0 1 13 4.25v2a.75.75 0 0 1-1.5 0v-2a.75.75 0 0 0-.75-.75h-5.5a.75.75 0 0 0-.75.75v11.5c0 .414.336.75.75.75h5.5a.75.75 0 0 0 .75-.75v-2a.75.75 0 0 1 1.5 0v2A2.25 2.25 0 0 1 10.75 18h-5.5A2.25 2.25 0 0 1 3 15.75V4.25Z" />
                         <path clipRule="evenodd" fillRule="evenodd" d="M19 10a.75.75 0 0 0-.75-.75H8.704l1.048-.943a.75.75 0 1 0-1.004-1.114l-2.5 2.25a.75.75 0 0 0 0 1.114l2.5 2.25a.75.75 0 1 0 1.004-1.114l-1.048-.943h9.546A.75.75 0 0 0 19 10Z" />
