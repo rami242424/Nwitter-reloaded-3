42db5d6e9786d88709b2effaedeca913ca92a53d
Rami <138556019+rami242424@users.noreply.github.com>
2026-01-22 21:54:17 +0900

    commit

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components"
 
 const Form = styled.form`
@@ -14,9 +15,9 @@ const TextArea = styled.textarea`
     background-color: black;
     width: 100%;
     resize: none;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     &::placeholder {
         font-size: 16px;
-        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     }
     &:focus {
         outline: none;
@@ -54,10 +55,16 @@ const SubmitBtn = styled.input`
 
 
 export default function PostTweetForm(){
+    const [isLoading, setIsLoading] = useState(false) ;
+    const [tweet, setTweet] = useState("");
+    const [file, setFile] = useState<File|null>(null);
+    const onChange = (e:React.ChangeEvent<HTMLTextAreaElement>) => {
+        setTweet(e.target.value);
+    }
     return <Form>
-        <TextArea placeholder="What is happening?"/>
+        <TextArea onChange={onChange} value={tweet} placeholder="What is happening?"/>
         <AttachFileButton htmlFor="file">Add photo</AttachFileButton>
         <AttachFileInput id="file" type="file" accept="image/*"/>
-        <SubmitBtn type="submit" value="Post Tweet"/>
+        <SubmitBtn type="submit" value={isLoading? "Posting..." : "Post Tweet"}/>
     </Form>
 }
