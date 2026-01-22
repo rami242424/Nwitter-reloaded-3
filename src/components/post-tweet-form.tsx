6fa0b62fbcef8e22e2dbf1597523f69b2f1528ca
Rami <138556019+rami242424@users.noreply.github.com>
2026-01-22 21:39:45 +0900

    commit

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,7 +1,29 @@
 import styled from "styled-components"
 
-const Form = styled.form``;
-const TextArea = styled.textarea``;
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+`;
+const TextArea = styled.textarea`
+    border: 2px solid white;
+    padding: 20px;
+    border-radius: 20px;
+    font-size: 16px;
+    color: white;
+    background-color: black;
+    width: 100%;
+    resize: none;
+    &::placeholder {
+        font-size: 16px;
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+    &:focus {
+        outline: none;
+        border-color: #1d9bf0;
+    }
+
+`;
 const AttachFileButton = styled.label``;
 const AttachFileInput = styled.input`
     display: none;
