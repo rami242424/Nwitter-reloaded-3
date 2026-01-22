06b6f476a59636fbc128e38cae0d1255b0223294
Rami <138556019+rami242424@users.noreply.github.com>
2026-01-22 21:00:12 +0900

    commit

---
+++ src/components/post-tweet-form.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+const Form = styled.form``;
+const TextArea = styled.textarea``;
+const AttachFileButton = styled.label``;
+const AttachFileInput = styled.input``;
+const SubmitBtn = styled.input``;
+
+
+export default function PostTweetForm(){
+    return <Form>
+        <TextArea placeholder="What is happening?"/>
+        <AttachFileButton htmlFor="file">Add photo</AttachFileButton>
+        <AttachFileInput id="file" accept="image/*"/>
+        <SubmitBtn />
+    </Form>
+}
