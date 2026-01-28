5821798f1ee65b755408e883a3b70a1c5127a53f
Rami <138556019+rami242424@users.noreply.github.com>
2026-01-28 11:21:20 +0900

    commit

--- src/components/routes/home.tsx
+++ src/components/routes/home.tsx
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 import PostTweetForm from "../post-tweet-form";
+import Timeline from "../timeline";
 
 const Wrapper = styled.div``;
 
 export default function Home(){
     return <Wrapper>
         <PostTweetForm />
+        <Timeline />
     </Wrapper>
 }
