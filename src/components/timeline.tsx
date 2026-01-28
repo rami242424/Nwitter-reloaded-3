e7a3d1235f9f8624204e00bc2d65fe9a25d4b0ca
Rami <138556019+rami242424@users.noreply.github.com>
2026-01-28 11:26:43 +0900

    commit

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,3 +1,14 @@
+import { useState } from "react";
+
+export interface ITweet {
+    photo: string;
+    tweet: string;
+    userId: string;
+    username: string;
+    createdAt: number;
+}
+
 export default function Timeline(){
+    const [tweet, setTweet] = useState<ITweet[]>([]);
     return <></>;
 }
