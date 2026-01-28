f0d9edb7f3955d1d4cbe0636ad0cfa0642c493e0
Rami <138556019+rami242424@users.noreply.github.com>
2026-01-28 17:23:58 +0900

    commit

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -45,7 +45,8 @@ const DeleteBtn = styled.button`
 export default function Tweet({username, photo, tweet, userId, id}: ITweet){
     const user = auth.currentUser;
     const OnDelete = async() => {
-        if(user?.uid !== userId)  return;
+        const ok = confirm("Are you sure you want to delete this tweet?");
+        if(!ok || user?.uid !== userId) return;
         try{
             await deleteDoc(doc(database, "tweets", id));
             if(photo){
@@ -55,7 +56,7 @@ export default function Tweet({username, photo, tweet, userId, id}: ITweet){
         } catch(e) {
             console.log(e)
         } finally {
-
+            ok
         }
     }
     return (
