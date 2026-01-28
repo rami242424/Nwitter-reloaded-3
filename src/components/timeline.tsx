a961d8e25b950805a4a6e3263b904b601c2c5302
Rami <138556019+rami242424@users.noreply.github.com>
2026-01-28 11:45:45 +0900

    commit

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,5 +1,7 @@
+import { collection, getDocs, orderBy, query } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { database } from "../firebase";
 
 export interface ITweet {
     photo: string;
@@ -14,7 +16,14 @@ const Wrapper = styled.div`
 `;
 export default function Timeline(){
     const [tweets, setTweet] = useState<ITweet[]>([]);
-    const fetchTweets = async() => {}
+    const fetchTweets = async() => {
+        const tweetsQuery = query(
+            collection(database, "tweets"),
+            orderBy("createdAt", "desc"),            
+        );
+        const snapshot = await getDocs(tweetsQuery);
+        snapshot.docs.forEach(doc => console.log(doc.data()));
+    }
     useEffect(() => {
         fetchTweets();
     }, []);
