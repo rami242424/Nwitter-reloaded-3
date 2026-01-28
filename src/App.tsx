1c8117a5e8deedf9d7950c3213ec0a9a7fe73afc
Rami <138556019+rami242424@users.noreply.github.com>
2026-01-28 17:26:51 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -22,9 +22,6 @@ body {
   color: white;
   font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
-::-webkit-scrollbar {
-display:none;
-}
 
 `;
 
