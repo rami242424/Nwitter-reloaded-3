da35ebc1d7552c7bce8aacdae2b849a40de956b4
Rami <138556019+rami242424@users.noreply.github.com>
2026-01-29 11:14:24 +0900

    commit

--- src/components/routes/profile.tsx
+++ src/components/routes/profile.tsx
@@ -1,3 +1,30 @@
+import styled from "styled-components"
+import { auth } from "../../firebase"
+
+const Wrapper = styled.div`
+    
+`;
+const AvatarUpload = styled.div`
+    
+`;
+const AvatarImg = styled.div`
+    
+`;
+const AvatarInput = styled.div`
+    
+`;
+const UserName = styled.div`
+    
+`;
+
 export default function Profile(){
-    return <h1>Profile</h1>;
+    const user = auth.currentUser;
+    //console.log(user, "user")
+    return <Wrapper>
+        <AvatarUpload>
+            <AvatarImg/>
+        </AvatarUpload>
+        <AvatarInput type="file" accept="image/*" />
+        <UserName>{user?.displayName ?? "Anonymous"}</UserName>
+    </Wrapper>
 }
